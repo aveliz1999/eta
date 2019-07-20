47ade66cb68acb630302a1dbf501073fd598b39f
aveliz1999 <aveliz1999@gmail.com>
2019-07-20 04:30:53 -0400

    Implement foreground location service

--- app/src/main/java/com/veliz99/eta/MainActivity.kt
+++ app/src/main/java/com/veliz99/eta/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.veliz99.eta
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.veliz99.eta.service.LocationService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener {
+            if(it.isSuccessful) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    startForegroundService(Intent(this, LocationService::class.java))
+                }
+                else{
+                    startService(Intent(this, LocationService::class.java))
+                }
+            }
+            else{
+                Toast.makeText(this, "Failed to create temporary user. Please try again.", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/veliz99/eta/service/LocationService.kt
+++ app/src/main/java/com/veliz99/eta/service/LocationService.kt
@@ -1,37 +1,141 @@
 package com.veliz99.eta.service
 
-import android.app.IntentService
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.app.*
 import android.content.Context
 import android.content.Intent
-import android.os.Build
+import android.location.Location
+import android.os.*
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import com.veliz99.eta.MainActivity
+import com.veliz99.eta.R
 
 private const val CHANNEL_ID = "location"
 private const val CHANNEL_NAME = "Location Update"
 private const val CHANNEL_DESCRIPTION = "Updates your location and traffic information while you drive"
 
-class LocationService : IntentService("ETA") {
+private const val NOTIFICATION_ID = 2799
 
-    override fun onHandleIntent(p0: Intent?) {
-        //startForeground(0, Notification.)
+private const val TAG = "LocationService"
 
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+private const val EXTRA_STARTED_FROM_NOTIFICATION = "com.veliz99.eta.service.startedFromNotification"
+
+class LocationService : Service() {
+
+    private lateinit var locationClient: FusedLocationProviderClient
+
+    private lateinit var notificationManager: NotificationManager
+
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+
+    private val binder = Binder()
+
+    private lateinit var documentId: String
+    private var currentLocation: Location = Location("dummyprovider")
+
+    override fun onCreate() {
+        Log.i(TAG, "Location service created")
+        locationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        documentId = FirebaseFirestore.getInstance()
+            .collection("etas")
+            .document().id
+
+        locationCallback = object: LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                super.onLocationResult(locationResult)
+                if(locationResult == null) {
+                    return
+                }
+                val location = locationResult.lastLocation
+                if(currentLocation.latitude != location.latitude || currentLocation.longitude != location.longitude) {
+                    currentLocation = location
+                    FirebaseFirestore.getInstance()
+                        .collection("etas")
+                        .document(documentId)
+                        .set(mapOf(
+                            "creator" to FirebaseAuth.getInstance().currentUser!!.uid,
+                            "location" to GeoPoint(location.latitude, location.longitude),
+                            "updated" to FieldValue.serverTimestamp()
+                        ))
+                }
+            }
+        }
+
+        locationRequest = LocationRequest().apply {
+            interval = 5000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
             channel.description = CHANNEL_DESCRIPTION
-            channel.enableLights(true)
-
             notificationManager.createNotificationChannel(channel)
         }
 
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("ETA Location Update")
-            .setContentText("Updating your location and ETA")
-        startForeground(1, builder.build())
+        requestLocationUpdates()
+    }
 
-        TODO("Implement location/traffic updating")
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(TAG, "Location service started")
+        val fromNotification = intent?.getBooleanExtra(EXTRA_STARTED_FROM_NOTIFICATION, false) ?: false
+
+        if(fromNotification) {
+            Log.i(TAG, "Stopping location updates")
+            locationClient.removeLocationUpdates(locationCallback)
+            stopSelf()
+        }
+        else {
+            startForeground(NOTIFICATION_ID, createNotification())
+        }
+
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        Log.i(TAG, "Service binding")
+        return binder
+    }
+
+    private fun createNotification(): Notification {
+        val intent = Intent(this, this::class.java)
+        intent.putExtra(EXTRA_STARTED_FROM_NOTIFICATION, true)
+
+        val servicePendingIntent = PendingIntent.getService(this, 0, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+        val serviceAction = NotificationCompat.Action(R.drawable.stop, "Stop", servicePendingIntent)
+
+        val activityPendingIntent = PendingIntent.getActivity(this, 0,
+            Intent(this, MainActivity::class.java), 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentText("Keeping your location up to date as your drive")
+            .setContentTitle("Updating location...")
+            .setOngoing(true)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentIntent(activityPendingIntent)
+            .addAction(serviceAction)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+
+        return builder.build()
+    }
+
+    private fun requestLocationUpdates() {
+        Log.i(TAG, "Requesting location updates")
+        try{
+            locationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+        }
+        catch (securityException: SecurityException) {
+            Log.e(TAG, "Lost location permission")
+        }
     }
 }
