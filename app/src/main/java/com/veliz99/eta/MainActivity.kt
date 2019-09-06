cb9c74ab3d65fb31660ddfa5c3aff3096293db2f
aveliz1999 <aveliz1999@gmail.com>
2019-09-06 16:48:35 -0400

    Add login check when starting service

--- app/src/main/java/com/veliz99/eta/MainActivity.kt
+++ app/src/main/java/com/veliz99/eta/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.veliz99.eta
 
-import android.content.Context
 import android.content.Intent
 import android.location.Address
-import android.location.Criteria
 import android.location.Geocoder
-import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AutoCompleteTextView
-import android.widget.MultiAutoCompleteTextView
 import androidx.appcompat.app.AlertDialog
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
@@ -63,7 +59,7 @@ class MainActivity : AppCompatActivity() {
                         .setTitle("Confirm Location")
                         .setMessage("Do you want to start navigating to " + address.getAddressLine(0) + "?")
                         .setPositiveButton("Yes") { _, _ -> startService(address.latitude, address.longitude)}
-                        .setNegativeButton("No") {_, _ -> setText("") }
+                        .setNegativeButton("No") { _, _ -> setText("") }
                         .create()
                     a.show()
                 }
@@ -72,36 +68,56 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startService(lat: Double, long: Double) {
-        FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener {
-            if(it.isSuccessful) {
-                val intent = Intent(this@MainActivity, LocationService::class.java)
-                intent.putExtra("latitude", lat)
-                intent.putExtra("longitude", long)
+        fun start() {
+            val intent = Intent(this@MainActivity, LocationService::class.java)
+            intent.putExtra("latitude", lat)
+            intent.putExtra("longitude", long)
 
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                    startForegroundService(intent)
-                }
-                else{
-                    startService(intent)
-                }
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                startForegroundService(intent)
+            }
+            else{
+                startService(intent)
+            }
 
-                val navigationUri = Uri.parse("google.navigation:q=$lat,$long")
-                val navigationIntent = Intent(Intent.ACTION_VIEW, navigationUri)
-                navigationIntent.`package` = "com.google.android.apps.maps"
-                if(navigationIntent.resolveActivity(packageManager) !== null) {
-                    startActivity(navigationIntent)
+            val navigationUri = Uri.parse("google.navigation:q=$lat,$long")
+            val navigationIntent = Intent(Intent.ACTION_VIEW, navigationUri)
+            navigationIntent.`package` = "com.google.android.apps.maps"
+            if(navigationIntent.resolveActivity(packageManager) !== null) {
+                startActivity(navigationIntent)
+            }
+            else{
+                this@MainActivity.let {
+                    val a = AlertDialog.Builder(it, R.style.ThemeOverlay_AppCompat_Dialog)
+                        .setTitle("Maps Missing")
+                        .setMessage("Cannot find the google maps app to launch navigation. Your location will still be tracked in the background.")
+                        .setNeutralButton("OK") { _, _ ->  }
+                        .create()
+                    a.show()
+                }
+            }
+        }
+
+        if(FirebaseAuth.getInstance().currentUser === null) {
+            FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener {
+                if(it.isSuccessful) {
+                    start()
                 }
                 else{
                     this@MainActivity.let {
                         val a = AlertDialog.Builder(it, R.style.ThemeOverlay_AppCompat_Dialog)
-                            .setTitle("Maps Missing")
-                            .setMessage("Cannot find the google maps app to launch navigation. Your location will still be tracked in the background.")
-                            .setOnCancelListener {  }
+                            .setTitle("Error Creating ETA")
+                            .setMessage("Cannot sign in to create the ETA. Please try again.")
+                            .setNeutralButton("OK") { _, _ ->  }
                             .create()
                         a.show()
                     }
                 }
             }
         }
+        else{
+            start()
+        }
+
     }
 }
