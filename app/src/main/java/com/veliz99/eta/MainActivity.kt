f5f3c4aa5503baee9fc0fd7dc5138d12a355217b
aveliz1999 <aveliz1999@gmail.com>
2019-09-11 01:05:37 -0400

    Implement and show my location button and auto zoom on open

--- app/src/main/java/com/veliz99/eta/MainActivity.kt
+++ app/src/main/java/com/veliz99/eta/MainActivity.kt
@@ -7,18 +7,27 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Address
+import android.location.Criteria
 import android.location.Geocoder
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.AutoCompleteTextView
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.marginBottom
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.veliz99.eta.adapter.AddressAdapter
@@ -47,12 +56,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupUi() {
         geocoder = Geocoder(this, Locale.getDefault())
+        val locationClient = LocationServices.getFusedLocationProviderClient(this)
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync {
             map = it
             it.isMyLocationEnabled = true
             it.uiSettings.isMyLocationButtonEnabled = true
+
+            map.setOnMyLocationButtonClickListener {
+                locationClient.lastLocation.addOnSuccessListener {
+                    val latLng = LatLng(it.latitude, it.longitude)
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                }
+                true
+            }
+
+            val myLocationButton = mapFragment.view!!.findViewWithTag<View>("GoogleMapMyLocationButton")
+            val myLocationButtonLayoutParams = myLocationButton.layoutParams as RelativeLayout.LayoutParams
+            myLocationButtonLayoutParams.removeRule(RelativeLayout.ALIGN_PARENT_TOP)
+            myLocationButtonLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+            myLocationButton.setPadding(myLocationButton.paddingLeft, myLocationButton.paddingTop, 25, 25)
+
+
             it.setOnMapLongClickListener {
                 val address = geocoder.getFromLocation(it.latitude, it.longitude, 1)[0] as Address
                 this@MainActivity.let {
@@ -66,6 +92,11 @@ class MainActivity : AppCompatActivity() {
                     a.show()
                 }
             }
+        }
+
+        locationClient.lastLocation.addOnSuccessListener {
+            val latLng = LatLng(it.latitude, it.longitude)
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
         }
 
         val autocompleteTextView = findViewById<AutoCompleteTextView>(R.id.autocompleteTextView_search)
